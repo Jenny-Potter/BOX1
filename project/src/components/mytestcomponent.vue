5bc3160747583c67ebf59af2010097853dee66f0
王敏 <569049370@qq.com>
2020-04-03 14:20:49 +0800

    init project

---
+++ project/src/components/mytestcomponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div>
+      i see
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {}
+  },
+
+  components: {
+  },
+
+  methods: {
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ project/src/pages/index/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Test />
+  </div>
+</template>
+
+<script>
+import Test from '@/components/mytestcomponent'
+export default {
+  data () {
+    return {}
+  },
+
+  components: {
+    Test
+  },
+
+  methods: {
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+</style>
